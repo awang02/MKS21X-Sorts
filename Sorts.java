5c20b1cf7e1e9f7b7e25bf26c523122f40e3486f
Allen Wang <awang02@stuy.edu>
2018-12-11 03:39:03 -0500

    Finished insertion sort

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,7 @@
 public class Sorts {
   /**Selection sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
+  *@param data the elements to be sorted.
   */
   public static void selectionSort(int [] ary){
     for (int i = 0; i < ary.length-1; i++){
@@ -21,7 +21,7 @@ public class Sorts {
 
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
+  *@param data the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
     for (int i = 0; i < data.length - 1; i++){
@@ -41,8 +41,26 @@ public class Sorts {
     }
   }
 
-  //Timothy so's driver
+  /**Insertion sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data the elements to be sorted.
+  */
+  public static void insertionSort(int[] ary){
+    int sortedIndex, currentVal;
+    for (int i = 1; i < ary.length-1; i++){
+      currentVal = ary[i];
+      int x = i-1;
+      while (x > 0 && ary[x] > currentVal){
+        ary[x+1] = ary[x];
+        x -= 1;
+      }
+      ary[x+1] = currentVal;
+    }
+  }
 
+
+  // ------------------------------
+  //Timothy So's driver
   public static String printArray(int[] ary) {
     String output = "[";
     for(int i=0; i<ary.length-1; i++) {
@@ -62,7 +80,7 @@ public class Sorts {
     System.out.println(printArray(test_a));               // [3, 4, 7, 1, 6, 2, 8, 6]
 
     //selectionSort(test_a);
-    bubbleSort(test_a);
+    //bubbleSort(test_a);
 
     sorted = printArray(test_a);
     System.out.println(sorted);               // [1, 2, 3, 4, 6, 6, 7, 8]
@@ -78,7 +96,7 @@ public class Sorts {
     System.out.println(printArray(test_b));   // [17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17]
 
     //selectionSort(test_b);
-    bubbleSort(test_b);
+    //bubbleSort(test_b);
 
     sorted = printArray(test_b);
     System.out.println(sorted);               // [1, 1, 2, 5, 11, 13, 17, 17, 18, 18, 19, 20]
@@ -94,7 +112,7 @@ public class Sorts {
     System.out.println(printArray(test_c));   // [14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52]
 
     //selectionSort(test_c);
-    bubbleSort(test_c);
+    //bubbleSort(test_c);
 
     sorted = printArray(test_c);
     System.out.println(sorted);               // [1, 9, 14, 15, 19, 30, 32, 33, 35, 37, 48, 50, 52, 57, 61, 65, 66, 85, 96, 100]
